a7284863719e763eb91d55cffef9c73e64380e9c
Danilo Santos <santosdanilo2024@gmail.com>
2023-03-17 22:23:14 -0300

    Criado App de Dado na Tabs3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  tamanho = 6;
+  valorAtual = 0;
+  imagesDados = [
+    'assets/assets/dice-red.png',
+    'assets/assets/dice.gif'
+  ]
+  imagemDado = this.imagesDados[0];
+  constructor() { }
+
+  jogarDado() {
+    this.valorAtual = 0;
+    this.imagemDado = this.imagesDados[1];
+    setTimeout(() => {
+      this.valorAtual = Math.floor(Math.random() * this.tamanho) + 1;
+    }, 1000);
+  }
 
 }
